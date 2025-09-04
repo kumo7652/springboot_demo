3d2284c7accc330e18791897305eaa35d9172c6b
kumo7652 <3347008977@qq.com>
2025-09-04 17:00:05 +0800

    [修改]修改配置信息

--- src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.neuedu.springboot_demo;
+package com.example.springboot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
